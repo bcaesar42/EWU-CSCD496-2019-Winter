56ec5eeca3453e2780a55d11fa7e94741f2b8f96
Bryan caesar <bryancaesar@eagles.ewu.edu>
2019-01-29 23:48:30 -0800

    Added "ValuesControllerTests" test class.

---
+++ SecretSanta/test/SecretSanta.Api.Tests/ValuesControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Api.Tests
+{
+    [TestClass]
+    public class ValuesControllerTests
+    {
+        
+    }
+}
