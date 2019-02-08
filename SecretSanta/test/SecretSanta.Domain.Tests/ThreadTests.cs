dbbe90866e6e07c6faf003091eb693f455469215
Bryan caesar <bryancaesar@eagles.ewu.edu>
2019-02-07 19:14:36 -0800

    Some class notes: 2-7-19.

---
+++ SecretSanta/test/SecretSanta.Domain.Tests/ThreadTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretSanta.Domain.Tests
+{
+    public class ThreadTests
+    {
+        //public async void Foo() // Avoid "async void".
+
+        public async Task Foo()
+        {
+            //var task = new Task(DoStuff);
+            //task.Start();
+
+            //var task = Task.Run(() => DoStuff());
+            //await task;
+
+            await Task.Run(() => DoStuff());
+        }
+
+        private void DoStuff()
+        {
+            
+        }
+
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod]
+        public async Task NeedToInvokeFoo()
+        {
+
+        }
+    }
+}
