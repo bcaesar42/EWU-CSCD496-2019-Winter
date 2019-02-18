7190f9798ed3da593fbad148fd886da5bbf18caa
Bryan caesar <bryancaesar@eagles.ewu.edu>
2019-02-17 23:00:46 -0800

    Added IPairingService.

---
+++ SecretSanta/src/SecretSanta.Domain/Services/Interfaces/IPairingService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretSanta.Domain.Services.Interfaces
+{
+    public interface IPairingService
+    {
+        Task<bool> GeneratePairings(int groupID);
+    }
+}
