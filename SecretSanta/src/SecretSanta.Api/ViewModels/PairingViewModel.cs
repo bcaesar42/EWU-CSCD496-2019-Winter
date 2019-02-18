f1667a3ea3da579b3ac62e19a1ee8b67f890e651
Unknown <bryancaesar@eagles.ewu.edu>
2019-02-17 23:01:29 -0800

    Added PairingViewModel.

---
+++ SecretSanta/src/SecretSanta.Api/ViewModels/PairingViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecretSanta.Api.ViewModels
+{
+    public class PairingViewModel
+    {
+        public UserInputViewModel Santa { get; set; }
+        public UserInputViewModel Recipient { get; set; }
+    }
+}
